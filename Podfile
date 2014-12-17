eed0abcb99759bff71a5946622676d2b9e92b9f1
Vitaly Berg <bergusman@gmail.com>
2014-12-17 20:58:01 +0300

    Add Crashlytics and Flurry

---
+++ Podfile
@@ -0,0 +1,4 @@
+platform :ios, '8.0'
+
+pod 'FlurrySDK', '~> 5.4'
+pod 'Reveal-iOS-SDK', '~> 1.0'
--- Ruble/AppDelegate.m
+++ Ruble/AppDelegate.m
@@ -8,13 +8,20 @@
 
 #import "AppDelegate.h"
 
+#import <Fabric/Fabric.h>
+#import <Crashlytics/Crashlytics.h>
+
 @interface AppDelegate ()
 
 @end
 
 @implementation AppDelegate
 
+#pragma mark - UIApplicationDelegate
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [Fabric with:@[CrashlyticsKit]];
+    
     return YES;
 }
 
